d2c26ff17b3031839bdda7f7eba41a8c56a00bf5
Pedro Castaneda <petecast76@gmail.com>
2024-01-12 03:05:20 -0800

    removed json and git files

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-const secrets = require('./secrets.json');
-const apiKey = secrets.API_KEY;
+const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=28e07edb830f62410b830383a942056d&page=1`;
 
-const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apiKey}&page=1`;
+const IMG_PATH = `https://image.tmdb.org/t/p/w1280`;
 
+// left a single quote at the end of the string to make it easier to add the search term
+const SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=28e07edb830f62410b830383a942056d&query="`;
+
+async function getMovies(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    console.log(data.results);
+}
+
+getMovies(API_URL);
